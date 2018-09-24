7e44931f0aa84177127f604f624c0235758ca8a4
Shankar Nagarajan <omshankar@gmail.com>
2018-09-24 11:07:07 +1000

    Made bridges persistant

--- linux_bridges/virbr_network.sh
+++ linux_bridges/virbr_network.sh
@@ -1,6 +1,15 @@
-virsh net-create virbr1.xml
-virsh net-create virbr2.xml
-virsh net-create virbr3.xml
-virsh net-create virbr4.xml
+virsh net-define virbr1.xml
+virsh net-autostart virbr1
+virsh net-start virbr1
+
+virsh net-define virbr2.xml
+virsh net-autostart virbr2
+virsh net-start virbr2
+
+virsh net-define virbr3.xml
+virsh net-autostart virbr3
+virsh net-start virbr3
+
+#virsh net-create virbr4.xml
 virsh net-list --all
 
